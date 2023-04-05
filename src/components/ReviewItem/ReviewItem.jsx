20c578351ce3a34a58931299a4de35cf9eb22924
topurayhan007 <topurayhan007@gmail.com>
2023-04-05 19:39:47 +0600

    order review done

---
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./ReviewItem.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashAlt, faTrashCan } from "@fortawesome/free-solid-svg-icons";
+
+const ReviewItem = ({ product }) => {
+  console.log(product);
+  const { id, img, name, price, quantity, shipping } = product;
+  return (
+    <div className="review-item">
+      <img src={img} alt="" />
+      <div className="review-details">
+        <p className="product-title">{name}</p>
+        <p>
+          Price: <span className="orange-text">${price}</span>
+        </p>
+        <p>
+          Order Quantity: <span className="orange-text">{quantity}</span>
+        </p>
+      </div>
+      <button onClick={() => handleRemoveFromCart(id)} className="btn-delete">
+        <FontAwesomeIcon className="delete-icon" icon={faTrashAlt} />
+      </button>
+    </div>
+  );
+};
+
+export default ReviewItem;
